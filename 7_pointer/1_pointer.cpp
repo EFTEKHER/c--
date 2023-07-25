75c2dcd2342f9956635b83b500dc5fffebc81993
EFTEKHER <eftekherali2000@gmail.com>
2023-07-25 22:29:22 +0600

    done

---
+++ 7_pointer/1_pointer.cpp
@@ -0,0 +1,24 @@
+/*
+code written by Eftekher Ali Efte from Bangladesh 
+ email:eftekherali2000@gmail.com
+pursuing computer science and engingeering in Ruet 
+*/
+#include<iostream>
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#define ull unsigned long long int
+#include<ctype.h> 
+using namespace std;
+      
+        
+int main()
+{
+    int x;
+    cout<<&x<<endl;
+    int *y=&x;
+    cout<<y<<endl;
+    cout<<&y<<endl;
+        
+return 0;
+}
